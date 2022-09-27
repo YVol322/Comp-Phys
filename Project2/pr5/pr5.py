61f148226e759a9275f7e27981df4e0ea7d0fa64
Yevhenii Volkov <111980535+YVol322@users.noreply.github.com>
2022-09-27 05:09:52 +0200

    Add files via upload

---
+++ Project2/pr5/pr5.py
@@ -0,0 +1,30 @@
+import csv
+import numpy as np
+from matplotlib import pyplot as plt 
+
+file = open("/Users/JVol/Documents/GitHub/comp-phys/project 2/pr5/pr5table.csv")
+type(file)
+csvreader=csv.reader(file)
+header = []
+header = next(csvreader)
+rows = []
+N = []
+j = []
+for row in csvreader:
+    rows.append(row)
+
+for i in range(len(rows)):
+    N.append(float(rows[i][0].strip()))
+    j.append(float(rows[i][1].strip()))
+
+f2 = np.polyfit(N,j, 2)
+
+
+plt.title("Number of itereations VS Number of rows/cos of A/R matrix for Jacobi's rotation algo")
+plt.xlabel("Numer of rows of A matrix N")
+plt.ylabel("Number of iterations J")
+plt.plot(N,j, "ob", label = "exact solutions")
+plt.plot(np.polyval(f2, N), label = "2nd order polynomial approximation")
+
+plt.legend()
+plt.show()
