4b7bc999bd317ad1eaed77460cba805352e0e15c
Yevhenii Volkov <veg2001@gmail.com>
2022-10-25 20:47:17 +0200

    a

--- project3/multiple_particles/100particles.cpp
+++ project3/multiple_particles/100particles.cpp
@@ -9,7 +9,7 @@ int main()
 {
     double q = 1;
     double m = 40.078;
-    int N = 5;
+    int N = 100;
 
     double B = 96.5;
     double V = 2.41 * pow(10, 6);
@@ -27,14 +27,12 @@ int main()
     t.v = v_init;
     t.t0 = 0.;
 
-    t.n = 400000.;
+    t.n = 4000.;
     t.total_time = 500;
     t.dt = t.total_time/t.n;
 
     t.setsize();
 
-    t.f = 0.1;
-    t.Wv = 0.2;
 
     for(int i = 0; i<N; i++)
     {
@@ -51,30 +49,56 @@ int main()
     t.r = r_init;
     t.v = v_init;
 
-    t.RK4(t.dt);
+    arma::mat W(48, 3);
+    arma::mat number(48, 3);
 
-    std::cout << t.number_of_particles(t.r) << std::endl;
-    std::cout << t.number_of_particles(r_init) << std::endl;
+    for(int i = 0; i<3; i++)
+    {
+        t.f = 0.1 + i * 0.3;
+        for(int j = 0; j<48;j++)
+        {  
+            t.Wv = 0.2 + 0.05 *j;
+            W(j,i) = t.Wv;
+            t.RK4(t.dt);
+            number(j,i) = t.number_of_particles(t.r);
 
-    //std::string filename = "FE4_table_100ms.csv";
-    //std::ofstream ofile;
-    //ofile.open("FE4_table_100ms.csv"); 
-//
-    //ofile <<"x1[μm], y1[μm], z1[μm], v_x1[μm/μs], v_y1[μm/μs], t[μs]" << std::endl;
-//
-    //int width = 15;
-    //int prec  = 6;
-//
-    //for(int i=0; i<t.n+1;i++)
-    //{
-    //    ofile << std::setw(width) << std::setprecision(prec) << std::scientific << t.x(i, 0) << ","
-    //    << std::setw(width) << std::setprecision(prec) << std::scientific << t.y(i, 0) << ","
-    //    << std::setw(width) << std::setprecision(prec) << std::scientific << t.z(i, 0) << ","
-    //    << std::setw(width) << std::setprecision(prec) << std::scientific << t.vx(i, 0) << ","
-    //    << std::setw(width) << std::setprecision(prec) << std::scientific << t.vy(i, 0) << ","
-    //    << std::setw(width) << std::setprecision(prec) << std::scientific << t.vz(i, 0) << ","
-    //    << std::setw(width) << std::setprecision(prec) << std::scientific << t.time(i) << std::endl; 
-    //}
+            std::cout << t.number_of_particles(t.r) << std::endl;
+            std::cout << t.number_of_particles(r_init) << std::endl;
+            std::cout << "i " << i << "j " << j << std::endl;
+
+            for(int k = 0; k<N; k++)
+                {
+                t.x(0, k) = r_init(0, k);
+                t.y(0, k) = r_init(1, k);
+                t.z(0, k) = r_init(2, k);
+
+
+                t.vx(0, k) = v_init(0, k);
+                t.vy(0, k) = v_init(1, k);
+                t.vz(0, k) = v_init(2, k);
+
+                t.r = r_init;
+                t.v = v_init;
+                }
+        }
+    }
+
+    std::string filename = "RK4_table_500m_50p.csv";
+    std::ofstream ofile;
+    ofile.open("RK4_table_500m_50p.csv"); 
+
+    ofile <<"n1, n2, n3, W" << std::endl;
+
+    int width = 7;
+    int prec  = 3;
+
+    for(int i=0; i<48;i++)
+    {
+        ofile << std::setw(width) << std::setprecision(prec) << std::scientific << number(i,0) << ","
+        << std::setw(width) << std::setprecision(prec) << std::scientific << number(i,1) << ","
+        << std::setw(width) << std::setprecision(prec) << std::scientific << number(i,2) << ","
+        << std::setw(width) << std::setprecision(prec) << std::scientific << W(i, 0) << std::endl; 
+    }
 
 
     return 0;
--- project3/multiple_particles/plot.py
+++ project3/multiple_particles/plot.py
@@ -1,68 +1,47 @@
 import csv
+from re import X
 import numpy as np
 from matplotlib import pyplot as plt 
 
-file = open("/Users/JVol/Documents/GitHub/comp-phys/project3/multiple_particles/FE4_table_100ms.csv")
+file = open("/Users/JVol/Documents/GitHub/comp-phys/project3/multiple_particles/RK4_table_500m_50p.csv")
 type(file)
 csvreader=csv.reader(file)
 header = []
 header = next(csvreader)
 rows = []
-x = []
-y = []
-z = []
-vx = []
-vy = []
-vz = []
-t = []
+n1 = []
+n2 = []
+n3 = []
+w = []
+
 
 for row in csvreader:
     rows.append(row)
 
 for i in range(len(rows)):
-    x.append(float(rows[i][6].strip()))
-    y.append(float(rows[i][7].strip()))
-    z.append(float(rows[i][8].strip()))
-    vx.append(float(rows[i][9].strip()))
-    vy.append(float(rows[i][10].strip()))
-    vz.append(float(rows[i][11].strip()))
-    t.append(float(rows[i][12].strip()))
+    n1.append(float(rows[i][0].strip()))
+    n2.append(float(rows[i][1].strip()))
+    n3.append(float(rows[i][2].strip()))
+    w.append(float(rows[i][3].strip()))
 
 plt.rcParams['font.size'] = '16'
 
 plt.figure(1)
 plt.xlabel('Time $t [ \mu s]$')
+plt.axis([0.2, 2.5, -3, 102])
 plt.ylabel('Coordinate $z [ \mu m]$')
-plt.plot(t,z, label = "RK4 solution", linewidth = 2, color = "r")
-plt.legend()
+plt.plot(w,n1,"ob",label = "RK4 solution", linewidth = 2, color = "r")
 
 plt.figure(2)
 plt.xlabel('Time $t [ \mu s]$')
+plt.axis([0.2, 2.5, -3, 102])
 plt.ylabel('Coordinate $x [ \mu m]$')
-plt.plot(t,x, label = "RK4 solution", linewidth = 2, color = "r")
-plt.legend()
+plt.plot(w,n2, "ob", label = "RK4 solution", linewidth = 2, color = "r")
 
 plt.figure(3)
 plt.xlabel('Time $t [ \mu s]$')
+plt.axis([0.2, 2.5, -3, 102])
 plt.ylabel('Coordinate $y [ \mu m]$')
-plt.plot(t,y, label = "RK4 solution", linewidth = 2, color = "r")
-plt.legend()
+plt.plot(w,n3, "ob", label = "RK4 solution", linewidth = 2, color = "r")
 
-plt.figure(4)
-plt.ylabel('Speed $V_x [ \mu m]$')
-plt.xlabel('Time $t [ \mu s]$')
-plt.plot(t,vx, label = "RK4 solution", linewidth = 2, color = "r")
-plt.legend()
-
-plt.figure(5)
-plt.ylabel('Speed $V_y [ \mu m]$')
-plt.xlabel('Time $t [ \mu s]$')
-plt.plot(t,vy, label = "RK4 solution", linewidth = 2, color = "r")
-plt.legend()
-
-plt.figure(6)
-plt.ylabel('Speed $V_z [ \mu m]$')
-plt.xlabel('Time $t [ \mu s]$')
-plt.plot(t,vz, label = "RK4 solution", linewidth = 2, color = "r")
-plt.legend()
 plt.show()
